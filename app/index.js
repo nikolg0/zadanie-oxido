a26e066d14a0e7441225ede5ffdc9dbcdf4c8b9d
nikolg0 <nikig0731@gmail.com>
2024-11-13 18:45:54 +0100

    read article

--- app/index.js
+++ app/index.js
@@ -1,7 +1,15 @@
-import OpenAI from "openai";
+const { OpenAI } = require("openai");
 const fs = require("fs");
 require("dotenv").config();
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+const readArticle = () => {
+  return fs.readFileSync("article.txt", "utf-8");
+};
+
+console.log(readArticle);
+
+console.log("hello");
