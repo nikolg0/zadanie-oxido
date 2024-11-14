e4f6eb78c30528a30b4e311f672c32d5f46f6515
nikolg0 <nikig0731@gmail.com>
2024-11-14 13:49:17 +0100

    another try

--- app/index.js
+++ app/index.js
@@ -27,7 +27,7 @@ const generateHtml = async () => {
         {
           role: "system",
           content:
-            "You are a helpful assistant provided with content, and your task is to structure it into HTML format with using proper tags like <h1>, <h2>, <p> and add image placeholders with <img> tags with src set to 'image_placeholder.jpg.' where appropriate. Make sure that each image have an alt attribute that includes a detailed prompt for generating the image, also use <figcaption> tag for caption added under each image. Generate the content that only fits between tags <body> </body>.",
+            "You are a helpful assistant provided with content, and your task is to structure it into HTML format with using proper tags like <h1>, <h2>, <p> and add image placeholders with <img> tags with src set to 'image_placeholder.jpg.' where appropriate. Make sure that each image have an alt attribute that includes a detailed prompt for generating the image, don't forget to use <figcaption> tags for captions added under each image. Generate the content that only fits between tags <body> </body>.",
         },
         {
           role: "user",
@@ -51,3 +51,5 @@ const htmlToFile = (content) => {
 };
 
 generateHtml();
+
+console.log("html wygenerowany");
