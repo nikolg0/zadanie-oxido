ee5a0b9dae34ab3627734a6066e006657acb095b
nikolg0 <nikig0731@gmail.com>
2024-11-14 15:05:04 +0100

    final touch

--- app/index.js
+++ app/index.js
@@ -25,7 +25,7 @@ const generateHtml = async () => {
         {
           role: "system",
           content:
-            "You are a helpful assistant provided with content, and your task is to structure it into HTML format with using proper tags like <h1>, <h2>, <p> and add image placeholders with <img> tags with src set to 'image_placeholder.jpg.' where appropriate. Make sure that each image have an alt attribute that includes a detailed prompt for generating the image, don't forget to use <figcaption> tags for captions added under each image. Generate the content that only fits between tags <body> </body>.",
+            "You are a helpful assistant provided with content, and your task is to structure it into HTML format with using proper tags like <h1>, <h2>, <p> and add image placeholders with <img> tags with src set to 'image_placeholder.jpg.' where appropriate. Make sure that each image have an alt attribute that includes a detailed prompt for generating the image, additionally add <figcaption> tags for captions added under each image. Remember to correct unknown Polish characters. Generate the content that only fits between tags <body> </body>.",
         },
         {
           role: "user",
@@ -45,9 +45,6 @@ const generateHtml = async () => {
 const htmlToFile = (content) => {
   const filePath = "app/artykul.html";
   fs.writeFileSync(filePath, content, "utf-8");
-  console.log("artykul.html zapisany");
 };
 
 generateHtml();
-
-console.log("html wygenerowany");
